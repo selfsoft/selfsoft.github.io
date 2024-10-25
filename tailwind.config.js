bed734bf09e0528fbd35d8fac961da336428d610
vlaim <vladislav.j.alexeyev@gmail.com>
2024-10-25 13:56:41 +0200

    Init layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'),
+          to: path.resolve(__dirname, 'webfonts')
+        }
+      ]
+    })
+  ],
 }
 
